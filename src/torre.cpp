aff73453475a0e5714c2d5d5fd46868ec42b43fb
LuisVicenteGordon <80354535+LuisVicenteGordon@users.noreply.github.com>
2025-05-08 16:32:03 +0200

    Update torre.cpp

--- src/torre.cpp
+++ src/torre.cpp
@@ -1,8 +1,13 @@
 #include "torre.h"
 
 
-bool torre::movimiento_valido(int x_origen, int y_origen, int x_destino, int y_destino, Pieza*** tablero) const {
-    if (x_origen != x_destino && y_origen != y_destino) return false; // Solo en línea recta
+bool torre::movimiento_valido(int x_origen, int y_origen, int x_destino, int y_destino, Pieza*** tablero, color jugadorColor) const {
+
+    if (tablero[x_origen][y_origen] == nullptr ||
+        tablero[x_origen][y_origen]->getColor() != jugadorColor) {
+        return false;
+    }
+    if (x_origen != x_destino && y_origen != y_destino) return false; // Solo en lÃ­nea recta
 
     int paso_x = (x_destino - x_origen) ? (x_destino > x_origen ? 1 : -1) : 0;
     int paso_y = (y_destino - y_origen) ? (y_destino > y_origen ? 1 : -1) : 0;
