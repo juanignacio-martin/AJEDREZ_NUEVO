565629b2463f6a923d05e83aa5906f49061f100f
LuisVicenteGordon <80354535+LuisVicenteGordon@users.noreply.github.com>
2025-05-08 16:18:07 +0200

    Update Peon.h

--- src/Peon.h
+++ src/Peon.h
@@ -1,19 +1,25 @@
 #pragma once
 #include "pieza.h"
 #include "tablero.h"
-
 class peon : public Pieza
 {
-private:
-    static int modo_juego; // 0 = Cl·sico/Demi, 1 = Silverman
+    static int modo_juego; // 0 = Cl√°sico/Demi, 1 = Silverman
+    static int ultima_fila_doble;
+    static int ultima_columna_doble;
+
+    static void registrarMovimientoDoble(int fila, int columna);
 
 public:
     peon(color col)
-        : Pieza(tipo_pieza::PEON, col)
+        :Pieza(tipo_pieza::PEON, col)
     {}
+    /*    void mueve(int x, int y)override {
+            std::cout << "muevo el peon" << x<<y<<std::endl;
+        }*/
 
-    bool movimiento_valido(int x_origen, int y_origen, int x_destino, int y_destino, Pieza*** tablero) const override;
+    bool movimiento_valido(int x_origen, int y_origen, int x_destino, int y_destino, Pieza*** tablero, color jugadorColor) const override;
+
     void promocionar(Pieza*** pieza_tablero, int x, int y);
 
-    static void setModoJuego(int modo); // Setter para cambiar el modo de juego
+    static void setModoJuego(int modo); // Cambia el modo de juego
 };
