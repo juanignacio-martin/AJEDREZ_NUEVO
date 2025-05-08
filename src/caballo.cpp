ceeaccbd60f25eeb52be39fbc918beb3750bca95
LuisVicenteGordon <80354535+LuisVicenteGordon@users.noreply.github.com>
2025-05-08 16:21:55 +0200

    Update caballo.cpp

--- src/caballo.cpp
+++ src/caballo.cpp
@@ -1,7 +1,13 @@
 #include "caballo.h"
 
 
-bool caballo::movimiento_valido(int x_origen, int y_origen, int x_destino, int y_destino, Pieza*** tablero) const {
+bool caballo::movimiento_valido(int x_origen, int y_origen, int x_destino, int y_destino, Pieza*** tablero, color jugadorColor) const {
+
+    if (tablero[x_origen][y_origen] == nullptr ||
+        tablero[x_origen][y_origen]->getColor() != jugadorColor) {
+        return false;
+    }
+
     int dx = abs(x_destino - x_origen);
     int dy = abs(y_destino - y_origen);
 
