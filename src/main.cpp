d5e0be34ef4728198055ef9a5515d957e77026f4
gmarianini <g.marianini@alumnos.upm.es>
2025-05-11 18:40:27 +0200

    eq

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,8 @@
 #pragma once
 #include <freeglut.h>
-<<<<<<< HEAD
-#include "ControladorJuego.h"
-#include "SeleccionVariante.h"
-
-ControladorJuego* controlador = nullptr;
-SeleccionVariante* selector = nullptr;
-=======
 #include "ControladorApp.h"
 
 ControladorApp* app = nullptr;
->>>>>>> main
 
 void displayCallback() {
     app->dibujar();
@@ -24,54 +16,7 @@ void keyboardCallback(unsigned char key, int x, int y) {
     app->manejarTecla(key, x, y);
 }
 
-void display() {
-    if (!selector->varianteConfirmada()) {
-        selector->dibujar();
-    }
-    else if (!controlador) {
-        partida* p = new partida(selector->getVariante());
-        controlador = new ControladorJuego(p);
-        // Aquí puedes hacer: glutDisplayFunc(...) si tienes otro render
-    }
-    else {
-        controlador->dibujar();  // Reemplaza por tu lógica real de juego
-    }
-}
-
-void mouse(int button, int state, int x, int y) {
-    if (!selector->varianteConfirmada()) {
-        selector->manejarClic(button, state, x, y);
-    }
-    // Podrías redirigir a controlador->manejarClic si el juego está activo
-}
-
-
 int main(int argc, char** argv) {
-<<<<<<< HEAD
-    std::string variante;
-    std::cout << "Elige variante (clasico, silverman, demi): ";
-    std::cin >> variante;
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(800, 600);
-    glutCreateWindow("Seleccion de Variante");
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0, 800, 0, 600);
-
-    selector = new SeleccionVariante(800, 600);
-
-    glutDisplayFunc(display);
-    glutMouseFunc(mouse);
-
-    glutMainLoop();
-    
-    partida* p = new partida(variante);
-    controlador = new ControladorJuego(p);
-
-=======
->>>>>>> main
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(800, 800); // se puede ajustar dinámicamente luego
